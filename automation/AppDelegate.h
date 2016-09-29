53222ac057d56840905d2853a17483c4ade5103c
itri_Apps <itri_apps@itri-appsdemacbook-pro.local>
2016-09-29 14:47:34 +0800

    Initial Commit

---
+++ automation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  automation
+//
+//  Created by itri_Apps on 2016/9/29.
+//  Copyright © 2016年 tw.org.itri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ automation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  automation
+//
+//  Created by itri_Apps on 2016/9/29.
+//  Copyright © 2016年 tw.org.itri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
